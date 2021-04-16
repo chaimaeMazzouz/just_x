5e91f4de3f4df66c93722900436bcafaa7348cd1
Boutissatne Issam <boutissante.issam.dev@gmail.com>
2021-04-16 21:44:12 +0200

    done

---
+++ Projet mines/Classes/Permis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_mines.Classes
+{
+    class Permis
+    {
+        public int id { get; set; }
+        public int num_demmande { get; set; }
+        public int num_permis{ get; set; }
+        public DateTime date_depot { get; set; }
+        public double investisement_projet { get; set; }
+        public string occupation_temporaire { get; set; }
+        public DateTime date_institition { get; set; }
+        public DateTime echeance { get; set; }
+        public double investisement_realise { get; set; }
+        public bool inscription_conservation { get; set; }
+        public DateTime date_depart_CRI { get; set; }
+        public DateTime date_retour_CRI { get; set; }
+        public DateTime date_decision { get; set; }
+        public DateTime date_enquete { get; set; }
+        public DateTime date_rapot { get; set; }
+    }
+}
