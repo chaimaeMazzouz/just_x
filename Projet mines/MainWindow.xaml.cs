ec3bfa27eab5ecdb91bda98d0497528b3b761ad9
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-04-18 14:35:16 +0000

    some modification

--- Projet mines/MainWindow.xaml.cs
+++ Projet mines/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Projet_mines
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
